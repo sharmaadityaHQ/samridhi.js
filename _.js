266a8e388cbe9ecd07b5bd8c915113be505071d9
JanviMahajan14 <janvimahajan337@gmail.com>
2019-08-01 17:01:18 +0530

    Added a reverse function

--- _.js
+++ _.js
@@ -1,32 +1,32 @@
 const _ = {
-  
-  clamp(number, lower, upper){
+
+  clamp(number, lower, upper) {
     let lowerClampedValue = Math.max(number, lower);
     let clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
   },
-  
-  inRange(number, start, end){
-    if(end === undefined){
+
+  inRange(number, start, end) {
+    if (end === undefined) {
       end = start;
       start = 0;
     }
-    if(start > end){
+    if (start > end) {
       let temp = end;
       end = start;
       start = temp;
     }
-    let isInRange = start<=number && number<end;
+    let isInRange = start <= number && number < end;
     return isInRange;
   },
-  
-  words(string){
+
+  words(string) {
     let words = string.split(' ');
     return words;
   },
-  
-  pad(string, length){
-    if(length <= string.length){
+
+  pad(string, length) {
+    if (length <= string.length) {
       return string;
     }
     let startPaddingLength = Math.floor((length - string.length) / 2);
@@ -34,65 +34,74 @@ const _ = {
     let paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
     return paddedString;
   },
-  
-  has(object, key){
+
+  has(object, key) {
     let hasValue = object[key];
-    if(object[key] === undefined){
+    if (object[key] === undefined) {
       hasValue = false;
     }
-    else if(object[key]){
+    else if (object[key]) {
       hasValue = true;
     }
     return hasValue;
   },
-  
-  invert(object){
+
+  invert(object) {
     let invertedObject = {};
-    for(let i in object){
+    for (let i in object) {
       let originalValue = object[i];
-      invertedObject = {originalValue : i};
+      invertedObject = { originalValue: i };
     }
     return invertedObject;
   },
-  
-  findKey(object, predicate){
-    for(let i in object){
+
+  findKey(object, predicate) {
+    for (let i in object) {
       let value = object[i];
       let predicateReturnValue = predicate(value);
-      if(predicateReturnValue){
+      if (predicateReturnValue) {
         return i;
       }
     }
     return undefined;
   },
-  
-  drop(array, n){
-    if(!n){
+
+  drop(array, n) {
+    if (!n) {
       n = 1;
     }
     let droppedArray = array.slice(n);
     return droppedArray;
   },
-  
-  dropWhile(array, predicate){
-    let dropNumber = array.findIndex(function(element, index){
+
+  dropWhile(array, predicate) {
+    let dropNumber = array.findIndex(function (element, index) {
       return !(predicate(element, index, array))
     });
     let droppedArray = this.drop(array, dropNumber);
     return droppedArray;
   },
-  
-  chunk(array, size){
-    if(size === undefined){
+
+  chunk(array, size) {
+    if (size === undefined) {
       size = 1;
     }
     let arrayChunks = [];
-    for(let i=0; i<array.length ; i+=size){
-      let arrayChunk = array.slice(i, i+size);
+    for (let i = 0; i < array.length; i += size) {
+      let arrayChunk = array.slice(i, i + size);
       arrayChunks.push(arrayChunk);
     }
     return arrayChunks;
+  },
+
+  reverse(array, size) {
+    let rev_array = [];
+    for (let i = size; i >= 0; i--) {
+      rev_array.push(array[i]);
+    }
+    return rev_array;
   }
+
 };
 
 // Do not write or modify code below this line
